ca02a15532c2e5f4ca969522555fe15c778fd28e
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2025-03-29 13:21:17 +0530

    create bidding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import SignUp from "./pages/SignUp";
 import Home from "./Home";
 import BookBidForm from "./pages/Deranindu/BookBidForm";
 import BidHome from "./pages/Deranindu/BidHome";
-import BiddingSuccess from "./pages/Deranindu/BiddingSuccess"; // Import BiddingSuccess page
-import ViewPlaceBid from "./pages/Deranindu/view-placebid"; // Import the new view-placebid component
+import BiddingSuccess from "./pages/Deranindu/BiddingSuccess";
+import ViewPlaceBid from "./pages/Deranindu/view-placebid";
+import Exbid from "./pages/Deranindu/Exbid";
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
         <Route path="/bidding" element={<BookBidForm />} />
         <Route path="/bidhome" element={<BidHome />} />
-        <Route path="/bidding-success" element={<BiddingSuccess />} /> {/* BiddingSuccess route */}
-        <Route path="/view-placebid" element={<ViewPlaceBid />} /> {/* New route for view-placebid */}
+        <Route path="/bidding-success" element={<BiddingSuccess />} />
+        <Route path="/view-placebid" element={<ViewPlaceBid />} />
+        <Route path="/exbid" element={<Exbid />} /> {/* Route to view bids */}
       </Routes>
     </Router>
   );
