31372b7ace151d77b7d47f9fd3cbcd0a6cb5c7c0
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2025-04-29 22:07:34 +0530

    initialize backend for bidding

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ import express from "express";
 import config from "./config.js";  // Changed from destructured import
 import mongoose from "mongoose";
 import { v2 as cloudinary } from 'cloudinary';
-import booksRoute from "./routes/bookRoutes.js";
-import bookRequestRoutes from "./routes/bookRequestRoutes.js";
-import exchangeRoutes from "./routes/exchangeRoutes.js";
+
 import cors from 'cors';
 
 const app = express();
@@ -15,20 +13,7 @@ const app = express();
 // Configure Cloudinary
 cloudinary.config(config.cloudinary);
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use("/uploads", express.static("uploads"));
 
-// Routes
-app.get('/', (req, res) => {
-    console.log("Received a request on '/' endpoint");
-    return res.status(200).send('Welcome to the MERN Stack Tutorial');
-});
-
-app.use('/books', booksRoute);
-app.use('/request', bookRequestRoutes);
-app.use("/exchange", exchangeRoutes);
 
 
 // Database connection
